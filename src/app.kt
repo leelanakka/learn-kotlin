d73f0a389bb0bfc507bd9907f173ba1c196fcd03
Nakka Leelaprasanth <44019459+leelanakka@users.noreply.github.com>
2019-06-05 11:35:22 +0530

    added maximum of two numbers function

--- src/app.kt
+++ src/app.kt
@@ -4,4 +4,10 @@ fun main() {
     val persons = listOf(Person("Alice"), Person("leela", 20))
     val oldest = persons.maxBy { some -> some.age ?: 0 }
     println("The oldest is : $oldest")
+    println("large number is : ${max(2,3)}")
+}
+
+fun max(a:Int,b:Int) :Int{
+    if (a>b) return a
+    return b
 }
